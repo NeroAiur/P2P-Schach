0c86904ab5033bf6cb49aef4387c96c8c05fa542
FelixJaeckel <fexlijaeckel@gmail.com>
2025-04-09 12:38:09 +0200

    views und urls basics/pseudoanfang

--- django/chess_app/urls.py
+++ django/chess_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    # IDEE, nicht finaler Code
+    path('/login', views.send_login_data_to_login_function, name="login"),
 ]
--- django/chess_app/views.py
+++ django/chess_app/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from backend.database_ops import add_user
 
 def index(request):
+    return render(request, "base.html")
+
+# PSEUDOCODE/IDEE
+def send_login_data_to_login_function(request):
+    username = request.GET.get('username')
+    password = request.GET.get('password')
+
+    # nicht ganz sicher, was dbpath ist
+    add_user(dbpath, username, password)
     return render(request, "base.html")
