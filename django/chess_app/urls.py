d61414cc7bde6cb6435a460e538914d9b85d5cab
FelixJaeckel <fexlijaeckel@gmail.com>
2025-04-11 11:04:48 +0200

    paar mehr urls und gen*eric* views

--- django/chess_app/urls.py
+++ django/chess_app/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 
-from . import views 
+from .views import * 
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', index, name="index"),
     # IDEE, nicht finaler Code
-    path('/login', views.send_login_data_to_login_function, name="login"),
+
+    path('game',         GameView.as_view(),       name="game"),
+    path('scoreboard',   ScoreboardView.as_view(), name='scoreboard'),
+    # path('profile/<ID>', ProfileView.as_view(),    name="profile"),
+
+    # path('login',                              send_login_data_to_login_function, name="login"),
+    # path('game/<room_<room_ID>/send_gamestate', send_gamestate,                    name="send_gamestate"),
+    # path('profile/<ID>/update_<elo>',          change_profile_name(),             name="change_elo"),
+
 ]
--- django/chess_app/views.py
+++ django/chess_app/views.py
@@ -1,8 +1,14 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 from backend.database_ops import add_user
 
+
 def index(request):
-    return render(request, "base.html")
+    return render(request, "index.html")
 
 # PSEUDOCODE/IDEE
 def send_login_data_to_login_function(request):
@@ -10,5 +16,14 @@ def send_login_data_to_login_function(request):
     password = request.GET.get('password')
 
     # nicht ganz sicher, was dbpath ist
-    add_user(dbpath, username, password)
+    add_user("db/user.db", username, password)
     return render(request, "base.html")
+
+class GameView(TemplateView):
+    template_name = "game.html"
+
+class ScoreboardView(TemplateView):
+    template_name = "scoreboard.html"
+
+class ProfileView(TemplateView):
+    template_name = "home.html"
